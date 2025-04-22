ebebf88fe09b57626def696135cb44920a9cbbcc
kflatner <kflatner3@gmail.com>
2025-04-22 19:23:26 +0200

    changed typo

--- View/playerView.js
+++ View/playerView.js
@@ -1,7 +1,7 @@
 function playerView() {
     return /*HTML*/ `
         <div class="player-container">
-            <button class="player-button" style="background-image: url('images/ola.png');" onclick="selectPlayer(2)"></button>
+            <button class="player-button" style="background-image: url('images/Ola.png');" onclick="selectPlayer(2)"></button>
             <button class="player-button" style="background-image: url('images/Vujan2.png');" onclick="selectPlayer(3)"></button>
             <button class="player-button" style="background-image: url('images/Kenneth2.png');" onclick="selectPlayer(1)"></button>
         </div>
